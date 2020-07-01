e76e34440b843a6be1ad1d34c12f93f029fb1e5d
Robert Calkins <rcalkins@umd.edu>
2020-07-01 17:49:03 -0500

    Completed the Tree interface

--- src/RBbst/RBbst.java
+++ src/RBbst/RBbst.java
@@ -1,13 +1,85 @@
 package RBbst;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
-/** <p>{@link RBbst} </p>
+/**
+ * <p>
+ * {@link RBbst}
+ * </p>
  *
  * @author Robert Calkins
  *
  * @see Tests
  */
 
-public class RBbst<T extends Comparable<T>> implements Tree<T> {
+public class RBbst<K extends Comparable<K>, V> implements Tree<K, V> {
+    private class node{
+        private K key;
+        private V value;
+        private node left,right;
+
+        
+
+    }
+    /**
+     * instance Variables
+     */
+
+    private int size;
     
+
+    public RBbst(){
+
+    }
+
+    @Override
+    public boolean put(K key, V value) throws InvalidParameterException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public V get(K key) throws InvalidParameterException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public V remove(K key) throws InvalidParameterException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) throws InvalidParameterException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(V value) throws InvalidParameterException {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public ArrayList<K> getKeys() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<V> getValues() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+
 }
--- src/RBbst/Tree.java
+++ src/RBbst/Tree.java
@@ -1,14 +1,91 @@
 package RBbst;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
 
-/** <p>{@link Tree} </p>
- *
+/** <p>{@link Tree} interface represents a generic treeMap class. All the relevent public methods that can be generalized to all types of trees are included within this 
+ * interface.</p>
  * @author Robert Calkins
  *
  * @see RBbst
  * @see Tests
  */
 
-public interface Tree<T extends Comparable<T>> {
+public interface Tree<K extends Comparable<K>, V> {
+
+
+    /**
+     * If the key does not exist within the tree, put will insert the key-value pair. Otherwise, it will update the value for the associated key given. 
+     * If key or value is null this function will throw an InvalidParameterException.
+     * @param key
+     * @param value
+     * @return True when inserting a new key, False when updating an existing key
+     * @throws InvalidParameterException
+     */
+    public boolean put(K key, V value) throws InvalidParameterException;
+
+    /**
+     * Retrieves the value associated with the given key. Throws InvalidParameterException if key is null.
+     * @param key
+     * @return the value associated with key
+     * @throws InvalidParameterException
+     */
+    public V get(K key) throws InvalidParameterException;
+
+    /**
+     * Will remove the node associated with the given key and return the associated value.
+     * 
+     * @param key
+     * @return
+     * @throws InvalidParameterException
+     */
+    public V remove(K key) throws InvalidParameterException;
+
+    /**
+     * 
+     * @param key
+     * @return true if key exists in the tree, false otherwise
+     * @throws InvalidParameterException
+     */
+    public boolean containsKey(K key) throws InvalidParameterException;
+
+    /**
+     * 
+     * @param value
+     * @return true if value exists in the tree, false otherwise
+     * @throws InvalidParameterException
+     */
+    public boolean containsValue(V value) throws InvalidParameterException;
+
+
+    /**
+     * @return An inorder representation of the key/value pair
+     */
+    @Override
+    public String toString();
+
+    /**
+     * @return An ArrayList of keys in sorted order
+     */
+    public ArrayList<K> getKeys();
+
+    /**
+     * 
+     * @return An ArrayList of values retrieved from an in-order traversal
+     */
+    public ArrayList<V> getValues();
+
+    /**
+     * 
+     * @return the size of the current true
+     */
+    public int getSize();
+
+  
     
+    
+
+
+
+
 }
